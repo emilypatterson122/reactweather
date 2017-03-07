0f35a915414bd6e3f5a092ae70a0a95cff2f45ed
Emily Patterson <emelocoton189@gmail.com>
2017-03-07 14:29:08 -0600

    Style About page and add links for React and OpenWeatherMap sites.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About</h1>
+      <p>I've built this for The Complete React Web App Developer Course!</p>
+      <p>Here are some of the tools I've used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JS framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - This was the API used to retrieve weather info based on location.
+        </li>
+      </ul>
     </div>
   )
 };
